aae40a90bb127aa44960130e55891661868f56ab
mario713 <richiedev92@gmail.com>
2018-10-26 17:43:26 +0200

    Small language correction

--- resources/views/settings/passchange.blade.php
+++ resources/views/settings/passchange.blade.php
@@ -5,7 +5,7 @@
         <div class="wrapper" style="max-width: 600px;">
             @foreach ($errors->all() as $error)
                 <div class="nNote nFailure hideit">
-                    <p><strong>Błąd: </strong>{{$error}}</p>
+                    <p><strong>Error: </strong>{{$error}}</p>
                 </div>
             @endforeach
         </div>
@@ -14,23 +14,23 @@
     @if(isset($success))
         <div class="wrapper" style="max-width: 600px;">
                 <div class="nNote nSuccess hideit">
-                    <p><strong>Sukces: </strong>{{ $success }}</p>
+                    <p><strong>Success: </strong>{{ $success }}</p>
                 </div>
         </div>
     @endif
 
-    <div class="title"><h5>Ustawienia Konta</h5></div>
+    <div class="title"><h5>Account Settings</h5></div>
 
     <!-- Headings -->
     <div class="widget first">
-        <div class="head"><h5 class="iLocked2">Zmiana Hasła</h5></div>
+        <div class="head"><h5 class="iLocked2">Password Change</h5></div>
 
         <form action="{{ url('/settings/passchange') }}" method="post" class="mainForm">
             <fieldset>
                 {{ csrf_field() }}
-                <div class="rowElem"><label>Aktualne Hasło:</label><div class="formRight"><input type="password" name="old_pass"></div></div>
-                <div class="rowElem"><label>Nowe Hasło:</label><div class="formRight"><input type="password" name="new_pass"></div></div>
-                <div class="rowElem"><label>Powtórz Nowe Hasło:</label><div class="formRight"><input type="password" name="confirm_new_pass"></div></div>
+                <div class="rowElem"><label>Your Password:</label><div class="formRight"><input type="password" name="old_pass"></div></div>
+                <div class="rowElem"><label>New Password:</label><div class="formRight"><input type="password" name="new_pass"></div></div>
+                <div class="rowElem"><label>Confirm New Password:</label><div class="formRight"><input type="password" name="confirm_new_pass"></div></div>
                 <div class="submitForm"><input type="submit" value="Zmień Hasło" class="basicBtn" /></div>
             </fieldset>
         </form>
--- resources/views/settings/userinfo.blade.php
+++ resources/views/settings/userinfo.blade.php
@@ -5,7 +5,7 @@
         <div class="wrapper" style="max-width: 600px;">
             @foreach ($errors->all() as $error)
                 <div class="nNote nFailure hideit">
-                    <p><strong>Błąd: </strong>{{$error}}</p>
+                    <p><strong>Error: </strong>{{$error}}</p>
                 </div>
             @endforeach
         </div>
@@ -14,30 +14,30 @@
     @if(isset($success))
         <div class="wrapper" style="max-width: 600px;">
             <div class="nNote nSuccess hideit">
-                <p><strong>Sukces: </strong>{{ $success }}</p>
+                <p><strong>Success: </strong>{{ $success }}</p>
             </div>
         </div>
     @endif
 
-    <div class="title"><h5>Ustawienia Konta</h5></div>
+    <div class="title"><h5>Account Settings</h5></div>
 
     <!-- Headings -->
     <div class="widget first">
-        <div class="head"><h5 class="iInfo">Informacje</h5></div>
+        <div class="head"><h5 class="iInfo">Info</h5></div>
 
         <form action="{{ url('/settings') }}" method="post" class="mainForm">
             {{ csrf_field() }}
             <fieldset>
-                <div class="rowElem"><label>Imie:</label><div class="formRight"><input type="text" name="first_name" @if (isset(Auth::user()->first_name)) value="{{ Auth::user()->first_name }}" @endif></div></div>
-                <div class="rowElem"><label>Nazwisko:</label><div class="formRight"><input type="text" name="last_name" @if (isset(Auth::user()->last_name)) value="{{ Auth::user()->last_name }}" @endif></div></div>
+                <div class="rowElem"><label>First Name:</label><div class="formRight"><input type="text" name="first_name" @if (isset(Auth::user()->first_name)) value="{{ Auth::user()->first_name }}" @endif></div></div>
+                <div class="rowElem"><label>Last Name:</label><div class="formRight"><input type="text" name="last_name" @if (isset(Auth::user()->last_name)) value="{{ Auth::user()->last_name }}" @endif></div></div>
                 <div class="rowElem">
-                    <label>Ukryj Imię i Nazwisko: </label>
+                    <label>Hide Name: </label>
                     <div class="formRight">
                         <span class="checked"><input type="checkbox" id="check1" name="hide_name" @if (Auth::user()->show_name == false) checked="checked" @endif></span>
                     </div>
                 </div>
                 <div class="rowElem">
-                    <label>Ukryj Email: </label>
+                    <label>Hide Email: </label>
                     <div class="formRight">
                         <span class="checked"><input type="checkbox" id="check1" name="hide_email" @if (Auth::user()->show_email == false) checked="checked" @endif></span>
                     </div>
