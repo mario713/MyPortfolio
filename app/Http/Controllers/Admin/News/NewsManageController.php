74520836e0072a05444dda447135c448ca7d79a9
mario713 <richiedev92@gmail.com>
2018-12-10 16:36:10 +0100

    Adding "use" namespace for Config::

--- app/Http/Controllers/Admin/News/NewsManageController.php
+++ app/Http/Controllers/Admin/News/NewsManageController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\HomeController;
 use App\News;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 
